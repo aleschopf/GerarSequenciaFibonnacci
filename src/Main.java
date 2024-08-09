c2f78e9ff7898fe931475f9cf1481f359e74d570
Alecsandro Schopf Auer Jr. <54159302+aleschopf@users.noreply.github.com>
2024-08-09 16:53:20 -0300

    docs: add comments on the gerarSequenciaFibonnacci method to clarify

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,20 @@ public class Main {
     }
 
     public static String gerarSequenciaFibonacci(int entradaUsuario) {
-        StringBuilder sequenciaFibonacci = new StringBuilder();
-        int primeiro = 0, segundo = 1;
+        StringBuilder sequenciaFibonacci = new StringBuilder(); // Criando um objeto StringBuilder para construir a sequência de Fibonacci
+        int primeiro = 0, segundo = 1; // Inicializando as variáveis 'primeiro' e 'segundo' com os primeiros valores da sequência de Fibonacci
 
-        if (entradaUsuario >= 0) {
-            sequenciaFibonacci.append(primeiro);
+        if (entradaUsuario >= 0) { // Verificando se o valor de 'entradaUsuario' é maior ou igual a 0
+            sequenciaFibonacci.append(primeiro); // Se for, adicionaremos o primeiro número da sequência (0) ao StringBuilder
         }
 
-        while (segundo <= entradaUsuario) {
-            sequenciaFibonacci.append(", ").append(segundo);
-            int proximo = primeiro + segundo;
-            primeiro = segundo;
-            segundo = proximo;
+        while (segundo <= entradaUsuario) { // Loop que continua enquanto 'segundo' for menor ou igual ao 'entradaUsuario'
+            sequenciaFibonacci.append(", ").append(segundo); // Adicionando o valor de 'segundo' ao StringBuilder, junto com uma vírgula e um espaço para separar
+            int proximo = primeiro + segundo; // Calculando o próximo número da sequência de Fibonacci como a soma de 'primeiro' e 'segundo'
+            primeiro = segundo; // Atualizando 'primeiro' com o valor de 'segundo'
+            segundo = proximo; // Atualizando 'segundo' com o valor de 'proximo'
         }
 
-        return sequenciaFibonacci.toString();
+        return sequenciaFibonacci.toString(); // Converte o StringBuilder para uma string e retorna a sequência de Fibonacci gerada
     }
 }
